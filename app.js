093105f27b4be1e50330aacc309d4d181b8da5f4
GonzaloGabrielBarrionuevo <gongbarrionuevo@gmail.com>
2021-01-18 16:51:22 -0300

    clogin and register created, index modified

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 // En la misma tendremos los archivos estáticos, como imágenes, css, etc.
 app.use(express.static('public'));
 
+
 // Levantamos nuestro servidor para que escuche peticiones al puerto 3000
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
@@ -18,6 +19,24 @@ app.get('/', (req,res)=>{
     // __dirname devuelve el directorio donde se encuentra el archivo actual (que es la carpeta principal / superior)
     // Devolvemos el archivo home.html, dentro /views
     // Especificando su ubicación desde la carpeta actual
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/index.html');
     // Es decir, cuando la app (nuestro servidor ejecutandose), reciba una petición get hacia la ruta base (/), devolveremos el archivo /home.html
 });
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+
